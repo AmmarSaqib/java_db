e1da6ff4fab8dc0f786a67f42af45422ae923a50
AmmarSaqib <ammarsaqib123@gmail.com>
2022-10-13 14:00:11 +0500

    added docstrings to the dbmanager

--- backend/java_service/java-db/src/main/java/com/venturenox/app/utils/dbmanagers/EmployeesDBMan.java
+++ backend/java_service/java-db/src/main/java/com/venturenox/app/utils/dbmanagers/EmployeesDBMan.java
@@ -13,11 +13,17 @@ public class EmployeesDBMan {
     EntityManager entity_manager;
     CriteriaBuilder criteria_builder;
 
+    // A constructor that takes an EntityManager as a parameter.
     public EmployeesDBMan(EntityManager entity_manager) {
         this.entity_manager = entity_manager;
         this.criteria_builder = this.entity_manager.getCriteriaBuilder();
     }
 
+    /**
+     * Get all the employees from the database.
+     * 
+     * @return A list of all the employees in the database.
+     */
     public List<Employee> get_all() {
         // using HQL
         this.entity_manager.getTransaction().begin();
@@ -31,6 +37,15 @@ public class EmployeesDBMan {
         return result;
     }
 
+    /**
+     * "Get all employees with the given id."
+     * 
+     * The first line of the function is a declaration of the return type. In this case, it's a list of
+     * employees
+     * 
+     * @param id The id of the employee you want to get.
+     * @return A list of employees with the given id.
+     */
     public List<Employee> get_by_id(int id) {
         // using Criteria Builder
         CriteriaQuery<Employee> query = this.criteria_builder.createQuery(Employee.class);
