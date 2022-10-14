f87c673d343cd421d58d684db87e9041e5775429
AmmarSaqib <ammarsaqib123@gmail.com>
2022-10-14 14:57:24 +0500

    removed mustache dont need it now

--- backend/java_service/java-db/src/main/java/com/venturenox/DbConfig.java
+++ backend/java_service/java-db/src/main/java/com/venturenox/DbConfig.java
@@ -1,11 +1,18 @@
 package com.venturenox;
 
 import jakarta.persistence.Persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
 public class DbConfig {
     String PERSISTENCE_UNIT;
+    String CONNECTION_URL = "jdbc:postgresql://" + System.getenv("POSTGRES_HOST") + "/" + System.getenv("DB_NAME");
+    String USERNAME = System.getenv("POSTGRES_USERNAME");
+    String PASSWORD = System.getenv("POSTGRES_PASSWORD");
     EntityManagerFactory ENTITY_MANAGER_FACTORY;
 
     // Initializing the persistence unit and initializing the entity manager
@@ -20,7 +27,13 @@ public class DbConfig {
      * > This function initializes the entity manager factory
      */
     private void init_entity_manager() {
-        this.ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory(this.PERSISTENCE_UNIT);
+        Map<String, String> persistenceMap = new HashMap<String, String>();
+
+        persistenceMap.put("javax.persistence.jdbc.url", this.CONNECTION_URL);
+        persistenceMap.put("javax.persistence.jdbc.user", this.USERNAME);
+        persistenceMap.put("javax.persistence.jdbc.password", this.PASSWORD);
+
+        this.ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory(this.PERSISTENCE_UNIT, persistenceMap);
     }
 
     /**
--- backend/java_service/java-db/src/main/java/com/venturenox/app/Main.java
+++ backend/java_service/java-db/src/main/java/com/venturenox/app/Main.java
@@ -5,20 +5,21 @@ import com.venturenox.app.model.dtos.Employee;
 import com.venturenox.app.utils.dbmanagers.EmployeesDBMan;
 
 import java.util.List;
-import java.io.FileInputStream;
 import java.util.Properties;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.apache.log4j.PropertyConfigurator;
 
-import jakarta.persistence.EntityManagerFactory;
-
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         // Configuring log4j via properties file
         Properties props_log = new Properties();
         try {
             props_log.load(new FileInputStream("/usr/app/log4j.properties"));
         } catch (Exception e) {
-            System.out.println(e.toString());
             System.out.println(e.toString());
         }
         PropertyConfigurator.configure(props_log);
@@ -26,12 +27,6 @@ public class Main {
         // intitializing the Database configuration and getting the entity manager
         // factory.
         DbConfig db = new DbConfig("my-persistence-unit");
-        EntityManagerFactory db_manager = db.get_factory();
-
-        // System.out.println(System.getenv("POSTGRES_USERNAME"));
-        // System.out.println(System.getenv("POSTGRES_PASSWORD"));
-        // System.out.println(System.getenv("POSTGRES_HOST"));
-        // System.out.println(System.getenv("DB_NAME"));
 
         List<Employee> result = new EmployeesDBMan(db.get_manager()).get_all();
         List<Employee> result_id = new EmployeesDBMan(db.get_manager()).get_by_id(27);
